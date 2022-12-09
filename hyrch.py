8314da9c31c32904b3a3c00de51a26795dff8c67
Pabxy <pabxy2020@gmail.com>
2022-12-09 17:33:59 +0100

    new file:   hyrch.py

---
+++ hyrch.py
@@ -0,0 +1,42 @@
+import os
+import time
+
+
+def check_distro():
+
+    global distro
+
+    if os.system("ls /bin | grep pacman "):
+        print("Distro derivada de Arch")
+        distro = "Arch"
+
+    elif os.system("ls /bin | grep apt "):
+        print ("Distro derivada de Debian")
+        distro = "Debian"
+
+    else:
+        print ("Distro linux no reconocida")
+        distro = ""
+
+def check_dependencies():
+
+    if os.system("ls /bin | grep git$ "):
+        print("Dependencia GIT satisfecha")
+
+    else:
+        print ("Dependencia GIT NO satisfecha")
+        print ("Instalando.....")
+        os.system("sudo pacman -S git --noconfirm")
+
+def login_manager():
+
+    #Install SDDM
+    os.system("sudo pacman -S sddm --noconfirm")
+
+    #Install SDDM-Theme
+    os.system("git clone https://github.com/surajmandalcell/Elegant-sddm.git")
+    os.system("cd Elegant-sddm")
+    os.system("./install.sh")
+
+        #Dependencies Theme
+    os.system("sudo pacman -S plasma-framework --noconfirm")
